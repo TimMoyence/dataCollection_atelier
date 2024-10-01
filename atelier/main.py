9e77f0d09088625402cb93c5075a7a4ca39b9592
Tim Kraken <tim.moyence@gmail.com>
2024-10-01 16:20:14 +0200

    add duckdb

---
+++ atelier/main.py
@@ -0,0 +1,7 @@
+import duckdb
+
+with duckdb.connect("file.db") as con:
+    con.sql("CREATE TABLE test (i INTEGER)")
+    con.sql("INSERT INTO test VALUES (42)")
+    con.table("test").show()
+    # the context manager closes the connection automatically
