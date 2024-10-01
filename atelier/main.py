8930730712602efc661fc8b9774e21811c556ec3
Tim Kraken <tim.moyence@gmail.com>
2024-10-01 16:58:38 +0200

    final import db

--- atelier/main.py
+++ atelier/main.py
@@ -1,13 +1,22 @@
 import duckdb
 
 with duckdb.connect("datalake.db") as con:
-    con.sql("CREATE TABLE test (i INTEGER)")
-    con.sql("INSERT INTO test VALUES (42)")
-    con.table("test").show()
-    # the context manager closes the connection automatically
-    
-    
+  con.sql("CREATE OR REPLACE TABLE Bordeaux_agenda AS SELECT * FROM read_csv_auto('./atelier/data/Bordeaux_agenda.csv',normalize_names=True)")
+  con.table('Bordeaux_agenda').show()
+  con.sql("CREATE OR REPLACE TABLE calendrier_scolaire AS SELECT * FROM read_csv_auto('./atelier/data/calendrier-scolaire.csv',normalize_names=True)")
+  con.table('calendrier_scolaire').show()
+  con.sql("CREATE OR REPLACE TABLE Lieux_parking AS SELECT * FROM read_csv_auto('./atelier/data/Lieux_parking.csv',normalize_names=True)")
+  con.table('Lieux_parking').show()
 
 
-duckdb.sql("CREATE OR REPLACE TABLE Bordeaux_agenda AS SELECT * FROM read_csv_auto('./atelier/data/Bordeaux_agenda.csv',normalize_names=True)")
-duckdb.table('Bordeaux_agenda').show()
+# import duckdb
+
+# with duckdb.connect("datalake.db") as db:
+#     db.sql('''
+#         CREATE OR REPLACE TABLE calendrier AS
+#         SELECT * FROM read_csv_auto(
+#         'calendrier.csv',
+#         normalize_names=True
+#         )
+#     ''')
+#     db.table('calendrier').show()
