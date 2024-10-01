7e4b244a9a2ead95e613cfa2581b8da1e8476a7c
Tim Kraken <tim.moyence@gmail.com>
2024-10-01 16:43:45 +0200

    data

--- atelier/main.py
+++ atelier/main.py
@@ -5,3 +5,8 @@ with duckdb.connect("file.db") as con:
     con.sql("INSERT INTO test VALUES (42)")
     con.table("test").show()
     # the context manager closes the connection automatically
+    
+    
+
+
+duckdb.sql("CREATE OR REPLACE TABLE nom_table AS SELECT * FROM read_csv_auto('./atelier/data/Bordeaux_agenda.csv',normalize_names=True)")
